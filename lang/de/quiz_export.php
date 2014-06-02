7996bc05e6025b74cc5e037a19266b39f995d45e
Johannes Burk <j.burk@joois.net>
2014-06-02 19:26:39 +0200

    deutsche Sprachdatei hinzugefügt

---
+++ lang/de/quiz_export.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Strings for component 'quiz_export', language 'de'
+ *
+ * @package   quiz_export
+ * @copyright 2014 Johannes Burk
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'QuizExport';
+$string['export'] = 'QuizExport'; // displayed in the navigation menu
+// $string['overviewfilename'] = 'QuizExport';
+
+$string['exportselected'] = 'Exportiere ausgewählte Versuche';
+
+$string['exportattemptcheck'] = 'Ausgewählte Versuche exportieren? (Dies kann je nach Anzhal der Fragen des Versuchs und Anzahl der ausgewählten Versuche zwischen ein paar Sekunden und Minuten dauern.)';
+
+$string['exportattempt'] = 'Versuch exportieren';
+
+// export_form
+$string['exportsettings'] = 'Export Einstellungen';
+$string['pagemode'] = 'Seitenumbruch (Seitenumbruch-Modus beim rendern des Quiz Reviews vor der Umwandlung zu PDF.)';
+$string['exportmodetruepage'] = 'Seiten wie in Quiz Einstellung';
+$string['exportmodequestionperpage'] = 'Eine Frage pro Seite';
+$string['exportmodesinglepage'] = 'Alle Fragen auf einer Seite';
