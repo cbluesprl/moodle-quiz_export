fd2050ab92d9c9227518fd3d4b782ed090e3ccf2
Johannes Burk <j.burk@joois.net>
2014-06-13 14:00:39 +0200

    added some comments for using addtional styles for convert to fix positionig and scaling problems with drag and drop questions. added a convert-style.css as example
    this changes adresses the problem #3

--- config.php
+++ config.php
@@ -21,8 +21,18 @@ defined('MOODLE_INTERNAL') || die();
 class quiz_export_config {
 	/**
 	 * The wkhtmltopdf command with optional options.
+	 * See wkhtmltopdf man page for detailed information. Some tips below:
+	 * 
 	 * --javascript-delay 1000 is recommended because some questiontypes uses javascript for rendering.
 	 * And otherwise some js files may be not loaded or ready with calculations until wkhtmltopdf starts rendering.
+	 * Increase this value if loading of scripts needs longer.
+	 * 
+	 * --user-style-sheet style.css allows to pass in additonal css styles
+	 * html {width: 800px;} recommended to avoid scaling and positioning problems.
+	 * (800 because wkhtmltopdf's screen resolution is hardcoded to 800x600)
+	 * 
+	 * e.g.:
+	 * "/usr/local/bin/wkhtmltopdf --javascript-delay 1500 --user-style-sheet convert-style.css"
 	 */
 	const WKHTMLTOPDF = "wkhtmltopdf";
 }
