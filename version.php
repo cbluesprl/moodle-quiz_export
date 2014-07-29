91d8094a77428e51c6bb37ae9c630d17e76c8120
Johannes Burk <j.burk@joois.net>
2014-07-29 18:09:14 +0200

    version bumped

--- version.php
+++ version.php
@@ -10,8 +10,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014061300;
+$plugin->version  = 2014072900;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '1.0.0 Beta 1 (Build 2014061300)';
+$plugin->release = '2.0.0 Beta 1 (Build 2014072900)';
 $plugin->component = 'quiz_export';
