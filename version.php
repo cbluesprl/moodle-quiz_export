65432b9dc43f6a9d24615b28f0245a965452930f
Johannes Burk <j.burk@joois.net>
2014-09-17 11:07:42 +0200

    version changed from beta to stable

--- version.php
+++ version.php
@@ -12,6 +12,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2014080400;
 $plugin->requires = 2014051200;
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '2.1.0 Beta 1 (Build 2014080400)';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.1.0 (Build 2014080400)';
 $plugin->component = 'quiz_export';
